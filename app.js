34ba6661b109cbb30548b30cc10207bc116a9a5f
Vargas <cristian.a.vargas@accenture.com>
2024-01-23 10:09:08 -0600

    moved MongoDB connection string to env file so it is hidden from GitHub

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes/user-routes';
 import blogRouter from './routes/blog-routes';
+import dotenv from "dotenv/config";
+const mongodbUri = process.env.MONGODB_URI;
 
 const app = express();
 app.use(express.json());
@@ -10,7 +12,7 @@ app.use('/api/blog', blogRouter);
 
 mongoose
     .connect(
-        "mongodb+srv://admin:53q08HHaNeoxbSJ6@cluster0.zaml2mp.mongodb.net/?retryWrites=true&w=majority"
+        `${mongodbUri}`
         )
         .then(() => app.listen(5000))
         .then(() => 
